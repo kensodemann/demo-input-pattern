9184b0d4b87f1e57b892c76375c77e477d029b0f
Ken Sodemann <ken@ionic.io>
2019-11-13 07:08:48 -0600

    convert

--- src/app/shared/numbers-only/numbers-only.directive.ts
+++ src/app/shared/numbers-only/numbers-only.directive.ts
@@ -10,17 +10,79 @@ export class NumbersOnlyDirective {
 
   @HostListener('input', ['$event'])
   onInput(event) {
+    this.convert(event.target);
     this.allowNumbers(event.target);
   }
 
   @HostListener('blur', ['$event'])
   onBlur(event) {
+    this.convert(event.target);
     this.allowNumbers(event.target);
   }
 
   @HostListener('focus', ['$event'])
   onFocus(event) {
+    this.convert(event.target);
     this.allowNumbers(event.target);
+  }
+
+  private convert(element: HTMLInputElement) {
+    let lastChar = element.value.substr(-1);
+    switch (lastChar) {
+      case 'q':
+      case 'Q':
+        lastChar = '1';
+        break;
+
+      case 'w':
+      case 'W':
+        lastChar = '2';
+        break;
+
+      case 'e':
+      case 'E':
+        lastChar = '3';
+        break;
+
+      case 'r':
+      case 'R':
+        lastChar = '4';
+        break;
+
+      case 't':
+      case 'T':
+        lastChar = '5';
+        break;
+
+      case 'y':
+      case 'Y':
+        lastChar = '6';
+        break;
+
+      case 'u':
+      case 'U':
+        lastChar = '7';
+        break;
+
+      case 'i':
+      case 'I':
+        lastChar = '8';
+        break;
+
+      case 'o':
+      case 'O':
+        lastChar = '9';
+        break;
+
+      case 'p':
+      case 'P':
+        lastChar = '0';
+        break;
+
+      default:
+        break;
+    }
+    element.value = element.value.substr(0, element.value.length - 1) + lastChar;
   }
 
   private allowNumbers(element: HTMLInputElement) {
